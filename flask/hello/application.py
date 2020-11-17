85063ef6d538ecbc0c1b7aa3afbea22aa5580b4e
loantran-Yemi <loantran.cs1706@gmail.com>
2020-11-17 23:17:43 +0700

    Commit 20201117 - Flask test render_template

--- flask/hello/application.py
+++ flask/hello/application.py
@@ -1,11 +1,14 @@
-from flask import Flask
+import random
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello, world!"
+    number = random.randint(0,1)
+    return render_template("index.html", number = number)
 
 @app.route("/goodbye")
 def bye():
-    return "Goodbye!"
+    return "goodbye"
+
